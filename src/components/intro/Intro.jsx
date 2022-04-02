8474781d8976ce58fec7434a0984f2c1a97b8a84
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-04-01 17:54:31 -0700

    intro and portfolio base done, added fake data

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import './intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+      console.log('textRef', textRef);
+      init(textRef.current, { 
+        showCursor: true, 
+        backDelay:  1500,
+        backSpeed: 60,
+        strings: ['Full Stack', 'React', 'Rails', 'Ruby' ] })
+
+  }, [])
+
+
   return (
     <div className="intro" id='intro'>
-      <div className="left"></div>
-      <div className="right"></div>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/FePaiva.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+          <div className="wrapper">
+            <h2>Hi There, I am </h2>
+            <h1> Felippe Paiva</h1>
+            <h3>Software Engineer <span ref={textRef}></span></h3>
+          </div>
+          <a href="#portfolio">
+              <img src="assets/down.png" alt="" />
+          </a>
+      </div>
     </div>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,88 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import './portfolio.scss'
+import PortfolioList from "../portfolioList/PortfolioList";
+import { glassmedsPortfolio,
+  slippyPortfolio,
+  instapauloPortfolio,
+  flatspacePortfolio,
+  blogPortfolio} from "../../data"
 
 export default function Portfolio() {
+
+    const [selected, setSelected]= useState("glassmeds")
+    const [data, setData]= useState([])
+
+    const list = [
+      {
+        id: "glassmeds",
+        title: "GlassMeds",
+      },
+      {
+        id: "slippy",
+        title: "Slippy Game",
+      },
+      {
+        id: "instapaulo",
+        title: "Insta Paulo Gallery",
+      },
+      {
+        id: "flatspace",
+        title: "Flatspace",
+      },
+      {
+        id: "blog",
+        title: "Blog",
+      },
+    ];
+
+    useEffect(()=>{
+
+      switch(selected){
+          case "glassmeds":
+          setData(glassmedsPortfolio);
+          break;
+          case "slippy":
+          setData(slippyPortfolio);
+          break;
+          case "instapaulo":
+          setData(instapauloPortfolio);
+          break;
+          case "flatspace":
+          setData(flatspacePortfolio);
+          break;
+          case "blog":
+          setData(blogPortfolio);
+          break;
+        default:
+          setData(glassmedsPortfolio)
+      }
+
+    }, [selected])
+
   return (
     <div className="portfolio" id='portfolio'>
-      Portfolio go here.
+      <h1>Portfolio</h1>
+      <ul>
+          {list.map(item=>(
+              <PortfolioList 
+              title={item.title} 
+              active={selected ===item.id} 
+              setSelected={setSelected}
+              id={item.id}
+              />
+          ))}
+      </ul>
+      <div className="container">
+            {data.map(d=>( 
+        <div className="item">
+          <img 
+          src={d.img}
+          alt="" 
+          />
+         <h3>{d.title}</h3>
+        </div>
+            ))}
+      </div>
     </div>
   )
 }
